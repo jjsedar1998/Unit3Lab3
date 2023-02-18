18ee9b1e90205afbeffd0090dcb13701a8786cb1
jjsedar1998 <91271580+jjsedar1998@users.noreply.github.com>
2023-02-18 13:18:56 -0500

    Update NewUnitTest1.cs

--- NewUnitTest1.cs
+++ NewUnitTest1.cs
@@ -7,22 +7,22 @@ namespace MyNewRootLibTest
 public class NewUnitTest1
 {
     [Fact]
-	public void TestPowerRootOf4() {
+	public void TestPowerOf4() {
 	
 		double a = 2; // arrange
 		double expected = 4;
 	
-		double actual = MyNewRootLib.Root.Power(a); // act
+		double actual = MyNewRootLib.Power.Power(a); // act
  
 		Assert.Equal(expected, actual); // assert
 	}	
 	[Fact]
-    public void TestPowerRootOf9() {
+    public void TestPowerOf9() {
     
 		double a = 3; // arrange
 		double expected = 9;
 	
-		double actual = MyNewRootLib.Root.Power(a); // act
+		double actual = MyNewRootLib.Power.Power(a); // act
 
 		Assert.Equal(expected, actual); // assert
 	} 
